3f7a662b4827ebce4259b457ee57015489ee7f6b
Han Verstraete <welteki@pm.me>
2021-05-19 23:08:56 +0200

    Add basic configuration

--- home.nix
+++ home.nix
@@ -9,6 +9,34 @@
   home.username = "han";
   home.homeDirectory = "/users/han";
 
+  programs = {
+    git = {
+      enable = true;
+      userName = "Han Verstraete";
+      userEmail = "welteki@pm.me";
+    };
+    
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      initExtra = ''
+        if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+          . ~/.nix-profile/etc/profile.d/nix.sh;
+          export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
+        fi # added by Nix installer
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+        '';
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    bat.enable = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
