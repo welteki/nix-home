2d3eb50d9099d4732eeeba2d31bed9fa3d6dfe31
Han Verstraete <welteki@pm.me>
2021-05-20 00:46:12 +0200

    zsh-syntax-highlighting

--- home.nix
+++ home.nix
@@ -20,6 +20,18 @@
       enable = true;
       enableCompletion = true;
       enableAutosuggestions = true;
+      plugins = [
+        {
+          name = "zsh-syntax-highlighting";
+          file = "zsh-syntax-highlighting.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.7.1";
+            sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
+          };
+        }
+      ];
       initExtra = ''
         if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
           . ~/.nix-profile/etc/profile.d/nix.sh;
